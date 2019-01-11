cbf46e2b47f2bd08db9ed84a27a2387236cb6485
Armen Arzumanyan <armena@YLT160015.CORP.OPTYM.NET>
2019-01-12 01:24:29 +0400

    update

--- auth/src/main/java/io/project/app/dto/Claim.java
+++ auth/src/main/java/io/project/app/dto/Claim.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
 
 /**
  *
@@ -17,7 +17,6 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 @ToString
 @EqualsAndHashCode
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Claim implements Serializable{
     
     private String iss;
