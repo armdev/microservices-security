8fd054225f0a2c90c5af06e2995bbbba73de2c95
Armen Arzumanyan <armena@YLT160015.CORP.OPTYM.NET>
2019-01-12 21:48:26 +0400

    update project

--- auth/src/main/java/io/project/app/auth/AuthApplication.java
+++ auth/src/main/java/io/project/app/auth/AuthApplication.java
@@ -1,5 +1,6 @@
 package io.project.app.auth;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
@@ -7,8 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"io.project"})
@@ -18,13 +21,21 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableEurekaClient
 public class AuthApplication {
 
-    
     public static void main(String[] args) {
         final SpringApplication application = new SpringApplication(AuthApplication.class);
         application.setBannerMode(Banner.Mode.CONSOLE);
         application.setWebApplicationType(WebApplicationType.REACTIVE);
         application.run(args);
-    }   
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Sampler sampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 
 }
-
