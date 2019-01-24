af11eaa03cc211fde57ef3ebbb774a999e71244b
armena <armen.arzumanyan@optym.com>
2019-01-24 17:18:43 +0400

    next push

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 echo "Build images"
-mvn clean package -U -Dmaven.test.skip=true
+#mvn clean package -U -Dmaven.test.skip=true
+sudo docker-compose up -d --no-deps --build mysql
+sudo docker-compose up -d --no-deps --build phpmyadmin
 sudo docker-compose build
 
 
